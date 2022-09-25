8050efad57add98952391583ab38d6346ae8cad9
Gerricane <sebuis16@gmail.com>
2022-09-25 19:01:26 +0000

    syntaxe

--- Exercice 1/Exercice1.py
+++ Exercice 1/Exercice1.py
@@ -1,6 +1,5 @@
 #Exercice de pyton n°1
 # https://python.developpez.com/cours/apprendre-python-3/?page=exercices-corriges#L14-2-1
-# Par gerricane
 
 from math import pi
 print("Programme de caclul de volume")
