f72b4a37c5d5c9c28b23c16f8f7e1830c0424823
Gerricane <sebuis16@gmail.com>
2022-09-25 18:42:09 +0000

    utilisation du module math

--- Exercice 1/Exercice1.py
+++ Exercice 1/Exercice1.py
@@ -1,13 +1,14 @@
 #Exercice de pyton n°1
 # https://python.developpez.com/cours/apprendre-python-3/?page=exercices-corriges#L14-2-1
 # Par gerricane
+
+from math import pi
 print("Programme de caclul de volume")
 hauteur = float(input("Donne une hauteur:"))
 rayon = float(input("Quel est le rayon du cercle:"))
 rayonCarre = rayon**2
 print(f'le rayon du cercle est de {rayon} et sont carré et de {rayonCarre}')
-Pi = 3.14
-print(f'Pi vaut {Pi}')
-volume = Pi * rayonCarre * hauteur / 3
+
+volume = pi * rayonCarre * hauteur / 3.0
 
 print(f'le volume du cone est de {volume}')
--- Exercice 1/README.md
+++ Exercice 1/README.md
@@ -1,7 +1,7 @@
 # Exercices n°1
 
-Pour calculer le volume d'un cône il faut:
-le rayon et la hauteur.
-A la formule il faut rajouter Pi
+Pour calculer le volume d'un cône il faut:<br>
+le rayon et la hauteur.<br>
+A la formule il faut rajouter Pi<br>
 
 source: https://calculis.net/volume/cone
