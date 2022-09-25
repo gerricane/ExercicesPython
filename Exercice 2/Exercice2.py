6fcf2f3c35407de461f882c3b5e8ded1615af96a
Gerricane <sebuis16@gmail.com>
2022-09-25 21:05:43 +0000

    ajout de la boucle

--- Exercice 2/Exercice2.py
+++ Exercice 2/Exercice2.py
@@ -1,6 +1,9 @@
 #Exercice n°2 
 #https://python.developpez.com/cours/apprendre-python-3/?page=exercices-corriges#L14-2-2
  
-prixHT = float(input("Entrez un prix hors taxe: "))
-prixTTC = prixHT * (1 + 0.20)
-print(f'Le prix TTC et de {prixTTC}€')
+prixHT = 1
+
+while prixHT >= 1:
+    prixHT = float(input("Entrez un prix hors taxe: "))
+    prixTTC = prixHT * (1 + 0.20)
+    print(f'Le prix TTC et de {prixTTC}€')
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # ExercicesPython
 Exerices de python du site developpez.com
+https://python.developpez.com/cours/apprendre-python-3/?page=exercices-corriges#L14-1
