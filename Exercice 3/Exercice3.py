4697fc1fc59f51ee2558bc7b58cc8c64f6b02d9f
Gerricane <sebuis16@gmail.com>
2022-11-10 19:24:03 +0100

    Fin de l'exo 3

--- Exercice 3/Exercice3.py
+++ Exercice 3/Exercice3.py
@@ -1,6 +1,15 @@
 # Exercice 3
 # https://python.developpez.com/cours/apprendre-python-3/?page=exercices-corriges#L14-2-3
 
-nombreUn = [5, 21, 120, -152, 121]
+somme, total, grands = 0, 0, 0
 
-nombreUn[4]
+x=int(input("Entre un nombre ou 0 pour quitter: "))
+while x > 0:
+    somme += x
+    total += 1
+    if x > 100:
+        grands += 1
+    x = int (input("x (0 pour terminer)"))
+    
+print("La somme est de", somme)
+print(total, "valeurs en touts et ",grands, " grand nombres")
