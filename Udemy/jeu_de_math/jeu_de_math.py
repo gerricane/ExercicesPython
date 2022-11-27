2103bdba0582e50d76b9a07535c8530103f96573
Gerricane <sebuis16@gmail.com>
2022-11-27 18:11:15 +0100

    Ajout cours Udemy

---
+++ Udemy/jeu_de_math/jeu_de_math.py
@@ -0,0 +1,33 @@
+import random
+
+NOMBRE_MIN = 1
+NOMBRE_MAX = 10
+NB_QUESTION= 4
+
+
+def poser_question():
+    a = random.randint(NOMBRE_MIN,NOMBRE_MAX)
+    b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
+    reponse_str = input(f"Calculez {a} + {b} = ")
+    reponse_int = int(reponse_str)
+    if reponse_int == a + b:
+        return True
+
+    return False
+
+
+
+#poser_question()
+
+nb_points = 0
+for i in range(0,NB_QUESTION):
+    print(f"Question n°{i+1} sur {NB_QUESTION}")
+    pts=poser_question()
+    if pts == True:
+        print("Réponse correcte")
+        nb_points += 1
+    else:
+        print("Réponse incorrect")
+    print()
+
+print(f"Votre note: {nb_points} sur {NB_QUESTION}")
