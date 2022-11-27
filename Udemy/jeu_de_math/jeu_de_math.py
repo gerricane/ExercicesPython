f2ea168fab292a324180140b7b06ba25d778ea27
Olivier Dulcy <olivier.dulcy@gmail.com>
2022-11-27 21:31:18 +0100

    Enhance `jeu_de_math.py`

--- Udemy/jeu_de_math/jeu_de_math.py
+++ Udemy/jeu_de_math/jeu_de_math.py
@@ -5,43 +5,39 @@ NOMBRE_MAX = 10
 NB_QUESTION= 5
 
 
-def poser_question():
+def poser_question() -> bool:
     a = random.randint(NOMBRE_MIN,NOMBRE_MAX)
     b = random.randint(NOMBRE_MIN, NOMBRE_MAX)
-    o =random.randint(0,1)
-    operateur_str = "+"
-    if o == 1:
-        operateur_str= "*"
-    reponse_str = input(f"Calculez {a} {operateur_str} {b} = ")
+    operators_str = ["+", "*"]
+    chosen_operator = random.choice(operators_str)
+    reponse_str = input(f"Calculez {a} {chosen_operator} {b} = ")
     reponse_int = int(reponse_str)
-    calcul = a+b
-    if o == 1:
-        calcul = a*b
-    if reponse_int == calcul:
-        return True
-    return False
+    if chosen_operator == "+":
+        calcul = a + b
+    else:
+        calcul = a * b
+    return reponse_int == calcul
 
 
 nb_points = 0
-for i in range(0,NB_QUESTION):
+for i in range(NB_QUESTION):
     print(f"Question n°{i+1} sur {NB_QUESTION}")
-    pts=poser_question()
-    if pts == True:
+    if poser_question():
         print("Réponse correcte")
         nb_points += 1
     else:
-        print("Réponse incorrect")
+        print("Réponse incorrecte")
     print()
 
 
 print(f"Votre note: {nb_points} sur {NB_QUESTION}")
-moyenne = int(NB_QUESTION/2)
+moyenne = NB_QUESTION // 2
 if nb_points == NB_QUESTION:
     print("Excellent")
 elif nb_points == 0:
-    print("Revisez vos maths")
+    print("Révisez vos maths")
 elif nb_points > moyenne:
-    print("pas mal")
+    print("Pas mal")
 else:
-    print("peut mieux faire")
+    print("Peut mieux faire")
 
