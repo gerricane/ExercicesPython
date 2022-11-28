bfb9158044978095178c4f752067375904e2e2b5
Gerricane <sebuis16@gmail.com>
2022-11-28 21:07:32 +0100

    Creation de l'exercice 1

--- Udemy/Section 9/Exercice 1/main.py
+++ Udemy/Section 9/Exercice 1/main.py
@@ -1,10 +1,34 @@
 # Exercice 1 de la section 9: Epreuve de code
 #Par Sebastien
 
+# Variable
 
-NB_POUCES = 2.54
-nb_a_convertir_str = input("Nombre a convetir en pouces: ")
-nb_a_convertir_flo = float(nb_a_convertir_str)
-resultat = nb_a_convertir_flo / NB_POUCES
+def conv_en_cm(nb_pouces):
+    print(nb_pouces)
+    return 0
 
-print(f"{nb_a_convertir_flo} cm donne {resultat}")
+def conv_en_pouces(nb_cm):
+    print(nb_pouces)
+    return 0
+
+
+menu_int = 0
+nb_en_pouces_floa = 0
+
+print("1: Conversion pouces vers cm")
+print("2: Conversion cm vers pouces")
+while menu_int == 0:
+    menu_str = input("Votre choix ? ")
+    try:
+        menu_int = int(menu_str)
+    except:
+        print("Il me faut un numero")
+
+if menu_int == 1:
+    while nb_en_pouces_floa == 0:
+        nb_en_pouce_str = input("Combien de pouces ? ")
+        try:
+            nb_en_pouces_floa = float(nb_en_pouce_str)
+        except:
+            print("J'attend un nombre")
+conv_en_cm(nb_en_pouces_floa)
