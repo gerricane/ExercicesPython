02ff1b5b1f05f26eda032453a2ee66415f6d989e
Gerricane <sebuis16@gmail.com>
2022-11-28 13:20:22 +0100

    Ajout de l'exo 1

---
+++ Udemy/Section 9/Exercice 1/main.py
@@ -0,0 +1,10 @@
+# Exercice 1 de la section 9: Epreuve de code
+#Par Sebastien Terriere
+
+
+NB_POUCES = 2.54
+nb_a_convertir_str = input("Nombre a convetir en pouces: ")
+nb_a_convertir_flo = float(nb_a_convertir_str)
+resultat = nb_a_convertir_flo / NB_POUCES
+
+print(f"{nb_a_convertir_flo} cm donne {resultat}")
