9da1fd0217236dbcd43a28be5096024d5dc740b8
Gerricane <sebuis16@gmail.com>
2022-12-04 17:56:02 +0100

    Section 16

--- Udemy/Section 16/main.py
+++ Udemy/Section 16/main.py
@@ -9,11 +9,23 @@ def afficher(collection):
         print()
         print(f"Premiere pizza: {collection[0]}")
         print(f"Derniere pizza: {collection[-1]}")
-# premiere pizza
-# derniere pizza
+
+
+def pizza_existe(collection,pizza_utilisateur):
+    for i in collection:
+        if i == pizza_utilisateur:
+            return True
+    return False
+
+def ajouter_pizza_utilisateur(collection):
+    pizza_utilisateur = input("Quelle est la pizza de votre choix ? ")
+    if pizza_existe(collection,pizza_utilisateur):
+        print("ERREUR: La pizza existe deja")
+    else:
+        collection.append(pizza_utilisateur)
+
 
 pizza = ["4 fromages", "vegetarienne", "hawai", "calzone"]
-#vide = ()
-# ajouter_pizza_utilisateur(pizza)
+ajouter_pizza_utilisateur(pizza)
 afficher(pizza)
 
