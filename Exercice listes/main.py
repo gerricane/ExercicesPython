f0934ac2239870d1b26ee9dbc1c6f1cc6fd2b6e8
Gerricane <sebuis16@gmail.com>
2022-12-07 22:40:17 +0100

    aaa

--- Exercice listes/main.py
+++ Exercice listes/main.py
@@ -1,12 +1,20 @@
 ma_liste = ["Laurent", "David", "Sébastien", "Jean-François", "Valerie"]
 
-print("Ma liste: ")
-print("--- --- --- ---")
-for i in range (len(ma_liste)):
-    print(ma_liste[i])
-print()
+def liste(une_liste, reponse_utilisateur = ""):
+    print("Ma liste: ")
+    print("--- --- --- ---")
+    for i in range(len(une_liste)):
+        print(i, une_liste[i])
+        if reponse_utilisateur == i:
+            print("Entrée deja presente")
+            return print("ERREUR")
+    une_liste.append(reponse_utilisateur)
+    print()
+
 
 reponse = input("Ajouter une nouvelle personne: ")
-ma_liste.append(reponse)
+liste(ma_liste,reponse)
 
-print(ma_liste)
+print("Ici une liste inversée")
+for i in ma_liste:
+    print(i)
